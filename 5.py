fab9581a714d4d50361eed01061b9fe8b6c45d92
LuisAngel1525 <48423098+LuisAngel1525@users.noreply.github.com>
2019-03-10 22:08:13 -0600

    Add files via upload

---
+++ 5.py
@@ -0,0 +1,38 @@
+from bs4 import BeautifulSoup
+import urllib3
+import pandas as pd
+from tabulate import tabulate
+import requests
+import re
+http=urllib3.PoolManager()
+
+urllib3.disable_warnings()
+
+url='https://es.wikipedia.org/wiki/Anexo:Presidentes_de_los_Estados_Unidos_por_edad'
+
+responde=requests.get(url).text
+
+
+soup=BeautifulSoup(responde, "html.parser")
+tabla = soup.find('table')
+
+
+name=""
+price=""
+Fila=0
+filas = tabla.find_all('tr')
+for fila in filas:
+    if Fila==1:
+        nroCelda=0
+        for celda in fila.find_all('td'):
+            if nroCelda==1:
+                name=celda.getText()
+                print("Precidente:", name)
+            if nroCelda==4:
+                price=celda.getText()
+                print("Valor:", re.sub("\D","",price))
+            nroCelda=nroCelda+1
+    else:
+        Fila=Fila+1
+
+
